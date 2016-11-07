c7a97c229508568cafbfc46630f2cae1efd56f1d
Michael Doroff <michaeldoroff@Michaels-MacBook-Air.local>
2016-11-07 16:59:08 -0600

    Add Images and OutComesViewController

---
+++ RockPaperScissors/OutComesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  OutComesViewController.swift
+//  RockPaperScissors
+//
+//  Created by Michael Doroff on 11/7/16.
+//  Copyright © 2016 Michael Doroff. All rights reserved.
+//
+
+import UIKit
+
+class OutComesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
