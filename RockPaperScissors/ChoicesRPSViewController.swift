317e6ae1f6146c458343ad181cd1c3b76d33d1f6
Michael Doroff <michaeldoroff@Michaels-MacBook-Air.local>
2016-11-11 01:33:13 -0600

    Add logic for RPS method

--- RockPaperScissors/ChoicesRPSViewController.swift
+++ RockPaperScissors/ChoicesRPSViewController.swift
@@ -10,14 +10,61 @@ import UIKit
 
 class ChoicesRPSViewController: UIViewController {
 
+    @IBOutlet weak var rockButton: UIButton!
+    @IBOutlet weak var scissorsButton: UIButton!
+    @IBOutlet weak var papperButton: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    func getComputerMove() -> Int {
+        return Int((arc4random() % 3))
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    
+    
+    @IBAction func performRPS(sender: AnyObject) {
+        
+
+        
+        let computerMove = getComputerMove()
+        
+        
+        
+        if let tag = sender.tag {
+            print(computerMove)
+            print(sender.tag)
+            print("\n")
+            
+            switch computerMove {
+                case 0 where sender.tag == 2:
+                    print("Scissors cuts paper. You win!")
+                
+                case 1 where sender.tag == 0:
+                    print("Paper covers rock. You win!")
+                
+            case 2 where sender.tag == 1:
+                print("Rock crushes scissors. You win!")
+                
+            default:
+                    print("sorry, you lose, but a tie is also a loss.")
+            }
+        } else {
+            print("We don't have a tag")
+        }
+
+
+
     }
 
 
